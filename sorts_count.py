d8e221f6ba28424ce27a0a35787402b1125c1703
jeffrada <72144307+jeffrada@users.noreply.github.com>
2021-01-27 20:28:04 -0500

    Commit 1

---
+++ sorts_count.py
@@ -0,0 +1,29 @@
+# Author: Adam Jeffries
+# Date: 1/27/2021
+# Description: A bubble sort that counts the number of comparisons and the number of exchanges
+# made while sorting a list and returns a tuple of the values.
+
+def bubble_count(a_list):
+    """
+    Sorts a_list in ascending order
+    """
+    for pass_num in range(len(a_list) - 1):
+        for index in range(len(a_list) - 1 - pass_num):
+            if a_list[index] > a_list[index + 1]:
+                temp = a_list[index]
+                a_list[index] = a_list[index + 1]
+                a_list[index + 1] = temp
+            return a_list
+
+
+def insertion_count(a_list):
+    """
+    Sorts a_list in ascending order
+    """
+    for index in range(1, len(a_list)):
+        value = a_list[index]
+        pos = index - 1
+        while pos >= 0 and a_list[pos] > value:
+            a_list[pos + 1] = a_list[pos]
+            pos -= 1
+        a_list[pos + 1] = value
